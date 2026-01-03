1ab135296d844381f7510e62a20e67cb3087159a
gtk <gatikbahet041211@gmail.com>
2026-01-03 14:07:04 +0530

    Update habits.js

---
+++ src/tracker/habits.js
@@ -0,0 +1,8 @@
+export let habits = [
+  { id: 'wake_6am', name: 'Wake up at 6AM', emoji: '⏰', goal: 30 },
+  { id: 'no_snooze', name: 'No Snoozing', emoji: '🚫', goal: 25 },
+  { id: 'drink_water', name: 'Drink 3L Water', emoji: '💧', goal: 30 },
+  { id: 'gym', name: 'Gym Workout', emoji: '🏋️', goal: 20 },
+  { id: 'stretch', name: 'Stretching', emoji: '🧘', goal: 30 },
+  { id: 'read', name: 'Read 10 Pages', emoji: '📚', goal: 30 }
+];
